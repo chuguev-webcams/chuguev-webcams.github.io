54987bb12aedaef8cccd4a04b08204b4cbb37d27
zmicc-19 <mrsas.ha.zm@gmail.com>
2020-08-30 23:53:20 +0300

    New scrollbar.

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
+// Scroll
 $(window).scroll(function () {
   var wintop = $(window).scrollTop(),
     docheight = $(document).height(),
     winheight = $(window).height();
   var scrolled = (wintop / (docheight - winheight)) * 100;
 
-  $(".scroll-line").css("width", scrolled + "%");
+  $("#progressbar").css("height", scrolled + "%");
+  $("#scrollPath").css("height", scrolled + "%");
 });
+// End scroll
 
+// Button refresh
 $(document).ready(function () {
   $(".button").click(function () {
     location.reload(true);
   });
 });
+// End button
 
+// Tabs
 $(document).ready(function () {
   $('a[data-toggle="tab"]').on("show.bs.tab", function (e) {
     localStorage.setItem("activeTab", $(e.target).attr("href"));
@@ -22,3 +28,4 @@ $(document).ready(function () {
     $('#myTab a[href="' + activeTab + '"]').tab("show");
   }
 });
+// End Tabs
